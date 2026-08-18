3b0574d9f1b6e606174e27d1a84c2ffc3ddfa435
Sneha-yadav0481 <sneha.25005058@jainuniversity.ac.in>
2026-08-18 23:28:28 +0530

    Add files via upload

---
+++ SnehaYadav0481.py
@@ -0,0 +1,84 @@
+
+name = "Sneha"
+age = 19
+height = 5.3
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+user_name = input("\nEnter your name: ")
+number = int(input("Enter a number: "))
+
+print("Hello", user_name)
+print("Your number is:", number)
+
+a = 2
+b = 4
+
+print("Arithmetic Operators")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+print("Floor Division:", a // b)
+
+
+print("Relational Operators")
+print(a > b)
+print(a < b)
+print(a == b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+
+x = True
+y = False
+
+print("Logical Operators")
+print(x and y)
+print(x or y)
+print(not y)
+
+print("Bitwise Operators")
+print("Bitwise And:", a & b)
+print("Bitwise Or:", a | b)
+print("Bitwise Not:", ~a)
+print("Bitwise Xor:", a ^ b)
+print("Bitwise Left shift operator:", a << b)
+print("Bitwise Right shift operator:", a >> b)
+
+c = 9
+
+print("Assignment Operators")
+
+c += 4
+print("Add and assign", c)
+
+c -= 4
+print("Subtract and assign", c)
+
+c *= 2
+print("Multiple and assign", c)
+
+c /= 1
+print("Divide and assign", c)
+
+c //= 2
+print("Floordivision and assign", c)
+
+c %= 1
+print("Modulus and assign", c)
+
+c **= 2
+print("Exponentiate and assign", c)
+
+
+print("Identity Operators")
+print(a is not b)
+print(a is b)
+
+print("Membership operators")
+print("a" in "apple")
